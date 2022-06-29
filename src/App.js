ebab62d8e87ad75ddaef01d61e2e955a7e145ae9
Noah Gordon <noah.gordon48@gmail.com>
2022-06-29 16:26:12 +1000

    added basic header (with links), footer and pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Link
+} from 'react-router-dom';
+import Home from './pages/Home'
+import AboutUs from './pages/AboutUs'
+import Events from './pages/Events'
+import ContactUs from './pages/ContactUs'
+import Resources from './pages/Resources'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      
+      <BrowserRouter>
+        <header className='header'>
+          
+          <Link to='/'>
+            <img className='logo' src='compclub_logo.png'/>
+          </Link>
+          <Link className='nav' to='/aboutus'>About Us</Link>
+          <Link className='nav' to='/events'>Events</Link>
+          <Link className='nav' to='/resources'>Resources</Link>
+          <Link className='nav' to='/contactus'>Contact Us</Link>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/aboutus' element={<AboutUs/>} />
+          <Route path='/events' element={<Events/>} />
+          <Route path='/contactus' element={<ContactUs/>} />
+          <Route path='/resources' element={<Resources/>} />
+        </Routes>
+      </BrowserRouter>
+      <footer className='footer'/>
     </div>
   );
 }
