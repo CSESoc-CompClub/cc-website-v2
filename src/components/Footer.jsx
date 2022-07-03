d861ab993aad2ee171c501c257875abc9ca2fcf8
Raghav Lall <z5309071@ad.unsw.edu.au>
2022-07-03 17:35:38 +1000

    added in new logos, made first steps to change footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+    
+      <img src='compclub-rainbow-logo.png' class='w-15 h-10' alt='' />
+    </footer>
+  );
+}
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React from 'react';
 
-export default function Events () {
-
+export default function Events() {
+  return (
+    <div>
+      <h3 class='mt-6 text-3xl font-sans font-bold text-center'>Workshops</h3>
+      <div class='mt-4 mx-12 text-center'>
+        <p>
+          Compclub runs a wide varierty of workshops such as VR, Gaming and 3D
+          modelling, taught through some of today's most popular programming
+          languages. We run workshops for both schools and organisations as well
+          as run our own public workshops hosted at UNSW.
+        </p>
+      </div>
+      <div class='mt-4 mx-12'>
+        <img src='events-collage.png' alt='' />
+      </div>
+    </div>
+  );
 }
