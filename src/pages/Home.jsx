f669059abb8864c19faf3f131e8c547f592d39e5
sohum299 <sohumjain8@gmail.com>
2022-07-04 23:06:45 +1000

    Homepage
    setting up homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Header from "../components/Header"
+import banner from "../assets/compclub_banner.png" 
+
+/*Header - Navbar
+ Banner - 1452 x 553 
+
+*/
 
 export default function Home () {
-
+    return (
+        <Header/>
+    )
 }
