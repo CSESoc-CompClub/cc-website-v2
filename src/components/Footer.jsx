f058010e9fc7d25153e50b42e723e905e9faa2ce
Raghav Lall <z5309071@ad.unsw.edu.au>
2022-07-05 21:54:45 +1000

    tweaked footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer>
-    
+    <footer class="p-4 bg-slate-900">
       <img src='compclub-rainbow-logo.png' class='w-15 h-10' alt='' />
+      <hr class="my-6 border-gray-200" />
+      <div class="text-sm text-white text-center">© 2022 CSESoc CompClub</div>
     </footer>
   );
 }
