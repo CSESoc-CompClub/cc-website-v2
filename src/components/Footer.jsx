45cfc6b9be14e39c86e256e77354b3f05c79eea5
Raghav Lall <z5309071@ad.unsw.edu.au>
2022-07-05 22:42:06 +1000

    adjusted image structure and imports

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import rainbowLogo from './../assets/general/compclub-rainbow-logo.png'
 
 export default function Footer() {
   return (
-    <footer class="p-4 bg-slate-900">
-      <img src='compclub-rainbow-logo.png' class='w-15 h-10' alt='' />
-      <hr class="my-6 border-gray-200" />
-      <div class="text-sm text-white text-center">© 2022 CSESoc CompClub</div>
+    <footer class='p-4 bg-slate-900'>
+      <img src={rainbowLogo} class='w-15 h-10' alt='' />
+      <hr class='my-6 border-gray-200' />
+      <div class='text-sm text-white text-center'>© 2022 CSESoc CompClub</div>
     </footer>
   );
 }
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import banner from './../assets/events/events-banner.png';
+import collage from './../assets/events/events-collage.png';
 
 export default function Events() {
   return (
     <div>
-      <h3 class='mt-6 text-3xl font-sans font-bold text-center'>Workshops</h3>
-      <div class='mt-4 mx-12 text-center'>
-        <p>
-          Compclub runs a wide varierty of workshops such as VR, Gaming and 3D
-          modelling, taught through some of today's most popular programming
-          languages. We run workshops for both schools and organisations as well
-          as run our own public workshops hosted at UNSW.
-        </p>
-      </div>
-      <div class='mt-4 mx-12'>
-        <img src='events-collage.png' alt='' />
+      <img src={banner} alt='' class='object-contain h-1/4' />
+      <div>
+        <h3 class='mt-6 text-3xl font-sans font-bold text-center'>Workshops</h3>
+        <div class='mt-4 mx-12 text-center'>
+          <p>
+            Compclub runs a wide varierty of workshops such as VR, Gaming and 3D
+            modelling, taught through some of today's most popular programming
+            languages. We run workshops for both schools and organisations as
+            well as run our own public workshops hosted at UNSW.
+          </p>
+        </div>
+        <div class='mt-4 mx-12'>
+          <img src={collage} alt='' />
+        </div>
       </div>
     </div>
   );
