223a88b9a153a0c30f54620c30a528059c5aacae
Zac Saber <z.saber@student.unsw.edu.au>
2022-07-15 17:38:04 +1000

    Added resource cards and updated links

---
+++ src/components/ResourceCard.jsx
@@ -0,0 +1,22 @@
+export default function ResourceCard() {
+  return (
+    <div>
+      <div class="shadow-md bg-sky-100 rounded-3xl"> 
+        <div class="shadow-md bg-sky-500 max-w-lg rounded-3xl"> 
+        <div class="border-b border-white-300 rounded-t-md p-4">
+          <div class="flex justify-center text-2xl text-white font-bold">Summer Workshop</div>
+        </div>
+        <div class="flex justify-center bg-sky-100 p-4">
+          Held at March 23, 2022. This workshop covers basic web-development (HTML, CSS) and Python!
+        </div>  
+        <div class="flex justify-center px-4 py-2 bg-sky-100 rounded-b-xl">
+          <a href="/shrek">
+          <button class="bg-violet-500 text-white hover:bg-green-500 hover:underline text-md font-normal rounded-xl px-2 py-1">
+            Link to Folder</button>   
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import Hero from "../components/Hero";
 import ArticleCard from "../components/ArticleCard";
+import ResourceCard from "../components/ResourceCard";
 
 export default function Resources() {
   return (
     <div>
       <Hero />
 
+    {/* Workshop Section */}
+      <div class="text-center py-5">
+        <h1 class="text-3xl gray-900 font-bold py-5">Resources</h1>
+        <p class="gray-500 py-5">i love compclub</p>
+        <div class="flex justify-center mb-3 bg-white-100 p-4">
+          <div class="flex flex-col justify-center space-y-4">
+            <div class="space-y-6">
+              {Array.from({ length: 2 }).map((_, index) => (
+                <ResourceCard key={index} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/*Articles Section */}
       <div class="text-center py-5">
         <h1 class="text-3xl gray-900 font-bold py-5">Articles</h1>
-        <p class="gray-500 py-5">Placeholder description</p>
+        <p class="gray-500 py-5">compclub dev is goated</p>
         <div class="flex flex-row justify-center space-x-4 py-5">
         {Array.from({ length: 3 }).map((_, index) => (
           <ArticleCard key={index} />
