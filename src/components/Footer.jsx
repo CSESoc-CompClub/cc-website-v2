ba37d4380f8934067176b31394091e5a0aac68fc
Raghav Lall <z5309071@ad.unsw.edu.au>
2022-09-17 17:28:21 +1000

    made header INCREDIBLY SEXY

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer class='p-4 bg-slate-900'>
       <img src={rainbowLogo} class='w-15 h-10' alt='' />
-      <hr class='my-6 border-gray-200' />
+      <hr class='my-6 border-gray-200'/>
       <div class='text-sm text-white text-center'>© 2022 CSESoc CompClub</div>
     </footer>
   );
